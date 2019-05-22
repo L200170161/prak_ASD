edac4c2a3bfac80e81c077ea9c63280bef59b2c5
MUHAMMAD HIMMAWAN <42951037+L200170161@users.noreply.github.com>
2019-05-23 00:00:10 +0700

    Add files via upload

---
+++ Queue Modul 8.4 .py
@@ -0,0 +1,86 @@
+import heapq
+
+'Nomor 4 '
+class Queue(object):
+    def __init__(self):
+        self.qlist = []
+    def isEmpty(self):
+        return len(self)==0
+    def __len__(self):
+        return len(self.qlist)
+    def enqueue(self,data):
+        self.qlist.append(data)
+    def dequeue(self):
+        assert not self.isEmpty()
+        return self.qlist.pop(0)
+    def getFront(self):
+        return self.qlist[-1]
+    def getRear(self):
+        return self.qlist[0]
+
+a = Queue()
+a.enqueue('qqqqq')
+a.enqueue('wwwww')
+a.enqueue('eeeee')
+
+print(a.qlist)
+a.dequeue()
+print(a.qlist)
+
+print(a.getFront())
+print(a.getRear())
+
+
+'Nomor 4 Prio'
+class Prior(object):
+    def __init__(self):
+        self.qlist= []
+    def __len__(self):
+        return len(self.qlist)
+    def isEmpty(self):
+        return len(self)==0
+    def enqueue(self, data, prior):
+        heapq.heappush(self.qlist, (prior, data))
+        self.qlist.sort()
+    def dequeue(self):
+        return self.qlist.pop(-1)
+    def getFront(self):
+        return self.qlist[-1]
+    def getRear(self):
+        return self.qlist[0]
+    
+a = Prior()
+
+a.enqueue('qqqqq', 5)
+a.enqueue('wwwww', 1)
+a.enqueue('eeeee', 2)
+
+print(a.qlist)
+a.dequeue()
+print(a.qlist)
+
+
+'Nomor 5'
+class Prior(object):
+    def __init__(self):
+        self.qlist= []
+    def __len__(self):
+        return len(self.qlist)
+    def isEmpty(self):
+        return len(self)==0
+    def enqueue(self, data, prior):
+        heapq.heappush(self.qlist, (prior, data))
+        self.qlist.sort()
+    def dequeue(self):
+        return self.qlist.pop(-1)
+    
+a = Prior()
+
+a.enqueue('qqqqq', 5)
+a.enqueue('wwwww', 1)
+a.enqueue('eeeee', 2)
+
+print(a.qlist)
+a.dequeue()
+print(a.qlist)
+
